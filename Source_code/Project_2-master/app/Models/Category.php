78fb9e7a68fd1b1617ff01cba3f3167cd1846468
Prommin Inkanha <prommin.i@kkumail.com>
2025-02-09 17:58:21 +0700

    ADD SourceCodeProject to branch

--- Source_code/Project_2-master/app/Models/Category.php
+++ Source_code/Project_2-master/app/Models/Category.php
@@ -5,7 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Traits\MultiLanguage;
+
 class Category extends Model
 {
-    use HasFactory
+    use HasFactory; 
 }
