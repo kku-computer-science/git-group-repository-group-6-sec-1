9e2fa9d0dd4150e813246c5e1bbc8993f6e63264
Prommin Inkanha <prommin.i@kkumail.com>
2025-02-09 19:17:01 +0700

    ADD SouceCodeProject

--- Source_code/Project_2-master/Project_2-master/app/Models/Category.php
+++ Source_code/Project_2-master/Project_2-master/app/Models/Category.php
@@ -5,8 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Traits\MultiLanguage;
-
 class Category extends Model
 {
-    use HasFactory; 
+    use HasFactory
 }
