a3b1a9f07ea17d020d3823f67ef6c22d4b93cd43
ikkyu78 <thanachai.f@kkumail.com>
2025-02-11 22:33:55 +0700

    test merge

--- Source_code/Project_2-master/Project_2-master/app/Models/ResearchGroup.php
+++ Source_code/Project_2-master/Project_2-master/app/Models/ResearchGroup.php
@@ -20,13 +20,6 @@ class ResearchGroup extends Model
     public function product(){
         return $this->hasOne(Product::class,'group_id');
     }
-<<<<<<< HEAD
 
-    //เพิ่มเติมให้  researchAssistants
-    public function researchAssistants()
-    {
-        return $this->hasMany(ResearchAssistant::class);
-    }
-=======
->>>>>>> origin/Thanakrit_2664
+
 }
