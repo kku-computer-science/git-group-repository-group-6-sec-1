8385038a7409e949eda48c5a0f8d5ba39b9efc6b
BodinSaensuk <bodin.s@kkumail.com>
2025-02-11 15:20:59 +0700

    Add UAT test: TC01, TC02, TC03 and Set up Project and Testing

---
+++ Source_Code/Project_2-master/Project_2-master/tests/Browser/TC15_EditResearchAssistantForm.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class TC15_EditResearchAssistantForm extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     */
+    public function testUserEditResearchAssistantForm()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('http://127.0.0.1:8000/login') // Open login page
+                    ->type('username', 'wongsar@kku.ac.th') // Fill in the Username field
+                    ->type('password', '123456789') // Fill in the Password field
+                    ->press('button[type="submit"]') // Click the submit button (by its type)
+                    ->assertPathIs('/dashboard'); // Check if redirected to the dashboard
+        });
+    }
+}
