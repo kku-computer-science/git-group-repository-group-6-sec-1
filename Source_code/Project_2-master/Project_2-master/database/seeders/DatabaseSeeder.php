8bad4e6ab3c505954d6d8fbfb68b682779e7cd5d
jirapatkol <jirapat.ko@kkumail.com>
2025-03-12 01:15:32 +0700

    fix databaseSeeder

--- Source_code/Project_2-master/Project_2-master/database/seeders/DatabaseSeeder.php
+++ Source_code/Project_2-master/Project_2-master/database/seeders/DatabaseSeeder.php
@@ -4,10 +4,6 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
-<<<<<<< HEAD
-
-=======
->>>>>>> origin/Thanakrit_2664
 class DatabaseSeeder extends Seeder
 {
     /**
